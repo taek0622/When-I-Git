5c084ccd6c29fa536b28b3b6356f4ae7097b20a2
taek0622 <min4644@gmail.com>
2023-03-29 21:32:10 +0900

    Feat:sparkles:: Add ContributionLevel Enum

--- when-i-git/Model.swift
+++ when-i-git/Model.swift
@@ -6,6 +6,30 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum ContributionLevel: String {
+    case none = "NONE"
+    case first = "FIRST_QUARTILE"
+    case second = "SECOND_QUARTILE"
+    case third = "THIRD_QUARTILE"
+    case fourth = "FOURTH_QUARTILE"
+
+    func fillColor(_ baseColor: AppColors) -> Color {
+        switch self {
+        case .none:
+            return Color(baseColor.rawValue + "0")
+        case .first:
+            return Color(baseColor.rawValue + "1")
+        case .second:
+            return Color(baseColor.rawValue + "2")
+        case .third:
+            return Color(baseColor.rawValue + "3")
+        case .fourth:
+            return Color(baseColor.rawValue + "4")
+        }
+    }
+}
 
 struct GithubModel: Codable {
     let data: GithubData
