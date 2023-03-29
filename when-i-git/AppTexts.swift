301189d3c461e31bb1745f843c12b95349d8a05e
taek0622 <min4644@gmail.com>
2023-03-29 21:31:04 +0900

    Feat:sparkles:: Add requestURL

---
+++ when-i-git/AppTexts.swift
@@ -0,0 +1,12 @@
+//
+//  AppTexts.swift
+//  when-i-git
+//
+//  Created by 김민택 on 2023/03/25.
+//
+
+import Foundation
+
+enum AppTexts {
+    static let requestURL = "https://api.github.com/graphql"
+}
