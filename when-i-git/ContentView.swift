066791f91621a483208646869bd8c70a0f37eb33
taek0622 <min4644@gmail.com>
2023-03-29 21:28:15 +0900

    Initialize Project

---
+++ when-i-git/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  when-i-git
+//
+//  Created by 김민택 on 2023/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ when-i-git/when_i_gitApp.swift
@@ -0,0 +1,19 @@
+//
+//  when_i_gitApp.swift
+//  when-i-git
+//
+//  Created by 김민택 on 2023/03/22.
+//
+
+import SwiftUI
+
+@main
+struct when_i_gitApp: App {
+    let networkManager = NetworkManager.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
