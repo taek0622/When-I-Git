57b419baaa6b0281057ab9618df1ea2d02a4ef5e
taek0622 <min4644@gmail.com>
2023-03-29 21:35:34 +0900

    Feat:sparkles:: Add HelpView

---
+++ when-i-git/HelpView.swift
@@ -0,0 +1,20 @@
+//
+//  HelpView.swift
+//  when-i-git
+//
+//  Created by 김민택 on 2023/03/23.
+//
+
+import SwiftUI
+
+struct HelpView: View {
+    var body: some View {
+        Text("HelpView")
+    }
+}
+
+struct HelpView_Previews: PreviewProvider {
+    static var previews: some View {
+        HelpView()
+    }
+}
